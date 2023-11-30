ca3fba997b07e82259ce156947aae04effa9b475
Aakash Yadav <ay9675928687@gmail.com>
2023-11-30 20:15:42 +0530

    learning typescript

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,11 @@
 let sales: number = 123_456_789;
 let course: string = "Typescript";
 let is_published: boolean = true;
+
+// let level;
+// level = 1;
+// level = "a";
+
+function render(document: any) {
+  console.log(document);
+}
