9cec09a5fc543695a05af8c1231aee9c13506b86
Aakash Yadav <ay9675928687@gmail.com>
2023-12-01 21:29:46 +0530

    learning typescript

--- dist/index.js
+++ dist/index.js
@@ -2,4 +2,21 @@
 let sales = 123456789;
 let course = "Typescript";
 let is_published = true;
+function render(document) {
+    console.log(document);
+}
+function calculateIncome(tax, year) {
+    if (tax > 50000) {
+        return tax * 50;
+    }
+    return `income is ${tax % 50}`;
+}
+calculateIncome(500000, 2019);
+let employee = { id: 1, name: "aakash" };
+console.log(employee);
+let weight;
+const textBox = {
+    drag: () => { },
+    resize: () => { },
+};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -15,3 +15,36 @@ let is_published: boolean = true;
 function render(document: any) {
   console.log(document);
 }
+
+function calculateIncome(tax: number, year: number): any {
+  if (tax > 50000) {
+    return tax * 50;
+  }
+  return `income is ${tax % 50}`;
+}
+calculateIncome(500000, 2019);
+
+let employee: { id: number; readonly name: string } = { id: 1, name: "aakash" };
+
+// employee.name = "vikas";
+console.log(employee);
+
+let weight: number & string;
+
+// intersection types
+
+type Draggable = {
+  drag: () => void;
+};
+type Resizeable = {
+  resize: () => void;
+};
+
+// declearing a intersection
+
+type UiWidget = Draggable & Resizeable;
+
+const textBox: UiWidget = {
+  drag: () => {},
+  resize: () => {},
+};
